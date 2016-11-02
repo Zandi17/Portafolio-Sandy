6b8ba61527e0c0d19629076709f00a01d75ff92c
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-11-02 17:21:29 -0500

    cambios en habilidades

--- js/app.js
+++ js/app.js
@@ -54,3 +54,15 @@ $('.navbar-collapse ul li a').click(function() {
 
 
 })( jQuery );
+
+
+$(document).ready(function() {
+  $('.html').css('width', '90%');
+  $('.css').css('width', '90%');
+  $('.jquery').css('width', '70%');
+  $('.javascript').css('width', '60%');
+  $('.adobe').css('width', '80%');
+  $('.wordpress').css('width', '10%');
+  $('.ui').css('width', '70%');
+  $('.rwd').css('width', '80%');
+});
